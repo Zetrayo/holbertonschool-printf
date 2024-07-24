807ee8b08d0550158795f31bdb1539155773677e
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-24 10:07:44 +0200

    Modified files to be able to merge

--- utilitaires.c
+++ utilitaires.c
@@ -1,25 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _strlen - Entry point
- * Description: 'counts string lenght'
- *
- * @a: pointer pointing to string
- * Return: 0 (Success)
- */
-
-int _strlen(char *a)
-{
-	int c = 0;
-
-	while (*a != '\0')
-	{
-		c++;
-		a++;
-	}
-	return (c);
-}
 
 /**
 * _putchar - Entry point
