3ad0a7d2f213ae9d733a470c802f73c871fbb869
Noziop <contact@fassih.ch>
2024-07-24 17:07:07 +0200

    EDIT : _printf.c : add the putchar function to limit the number of files EDIT : print_functions.c : moving the ROT13 function inside, to have 5 functions in it DEL : print_functions_advanced.c : empty since ROT13 moved in print_functions.c DEL : utility.c : empty since _putchar() moved in _printf.c file.

--- _printf.c
+++ _printf.c
@@ -83,3 +83,17 @@ int handle_format(char spec, format_t *formats, va_list args,
 	buffer[(*buffer_index)++] = spec;
 	return (2);
 }
+
+#include "main.h"
+
+/**
+* _putchar - Entry point
+*
+* @c: character to print
+* Return: Always 0 (Success)
+*/
+int _putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
