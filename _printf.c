17ffdb6f34b4afc848b1ef03fe86c46786147500
Zetrayo <josefeliciano.com98@gmail.com>
2024-07-25 11:39:43 +0200

    FIX : README.md, fixed some spelling mistakes FIX : _printf.c, added condition for octal on line 35 FIX : main.h, added prototype for octal on line 32 ADD : more_print_functions.c, added a file containing the function for octal

--- _printf.c
+++ _printf.c
@@ -32,6 +32,7 @@ int _printf(const char *format, ...)
 		{'d', print_int},
 		{'i', print_int},
 		{'R', print_rot13string},
+        {'o', print_octal},
 		{0, NULL}
 	};
 
---
+++ more_print_functions.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_octal - Prints an integer
+ * @args: List of arguments
+ * @buffer: Buffer array to handle print
+ * @buffer_index: Index at which to add next char, represents the length
+ *
+ * Return: Number of chars printed
+ */
+int print_octal(va_list args, char *buffer, int *buffer_index)
+{
+	int n = va_arg(args, int);
+	int count = 0, i = 0, num = 0;
+	char octal[BUFFER_SIZE];
+
+	while (n > 0)
+	{
+		num = n % 8;
+		n = n / 8;
+		octal[i] = num + '0';
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		buffer[(*buffer_index)++] = octal[i];
+		count++;
+		if (*buffer_index == BUFFER_SIZE)
+		{
+			flush_buffer(buffer, buffer_index);
+		}
+	}
+	return (count);
+}
+
+/*
+%o print a number in octal (base 8)
+%u print an unsigned decimal (base 10) number
+%x print a number in hexidecimal (base 16) , letters in lowercase
+%X print a number in hexidecimal (base 16) , letters in uppercase
+*/
