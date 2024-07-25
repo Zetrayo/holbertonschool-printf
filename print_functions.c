b56e43e1e0988800ec36f1e15a38ec0b6f33e0aa
Noziop <contact@fassih.ch>
2024-07-25 16:13:48 +0200

    Revert to 083ace62f74a56ed2abfb9e692cfd276cd441abe

--- print_functions.c
+++ print_functions.c
@@ -81,7 +81,7 @@ int print_string(va_list arg,  char *buffer, int *buffer_index)
 	if (str == NULL)
 		str = "(null)";
 
-	while (*str)
+	while (str[count])
 	{
 		buffer[(*buffer_index)++] = str[count];
 		if (*buffer_index == BUFFER_SIZE)
